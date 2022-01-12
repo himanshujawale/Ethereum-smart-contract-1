eb14d69b723310d7c971e8cf081967507e74803d
Arjun Shrivas <97286040+Microservices-delixus@users.noreply.github.com>
2022-01-12 22:25:15 +0530

    Script to save mythril analyze output insdie logfile

--- Save_logs.sh
+++ Save_logs.sh
@@ -2,15 +2,14 @@
 filenames=`ls -l solidity_files | grep '^-' | awk '{print $9}'`
 [ -d "data_log" ] && echo "Mythril analyzing..." || echo -e " Directory Creating.... \n Mythril analyzing..." $(mkdir data_log) 
 
-temp=1
 for eachfile in $filenames
 do
   if [ "${eachfile: -4}" == ".sol" ]  #check sol file extension. 
   then
 
-        myth analyze solidity_files/$eachfile &> log_$temp   # execute myth analyze on Smart Contract  and save result inside  log file
+        myth analyze solidity_files/$eachfile &> ${eachfile%.*}_log   # execute myth analyze on Smart Contract  and save result inside  log file
         mv log_$temp data_log/                # move all log file in data_log folder 
         echo ${eachfile%.*}_log file created successfully. 
   fi
-  ((temp=temp+1))
+  
 done
