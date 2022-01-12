8d97a5ac17e6671ddf9f48009a352b50a7c1d9e6
Arjun-Shrivas <arjunshrivas1997@gmail.com>
2022-01-12 09:26:44 +0000

    Shell Script To Save Result Output inside log file

---
+++ Save_logs.sh
@@ -0,0 +1,15 @@
+filenames=`ls -l | grep '^-' | awk '{print $9}'`
+[ -d "data_log" ] && echo "ok" || echo -e "Data log \n Directory Creating...." $(mkdir data_log) 
+
+temp=1
+for eachfile in $filenames
+do
+  if [ "${eachfile: -4}" == ".sol" ]  #check sol file extension. 
+  then
+
+        myth analyze $eachfile &> log_$temp   # execute myth analyze on Smart Contract  and save result inside  log file
+        mv log_$temp data_log/                # move all log file in data_log folder 
+        echo task $temp Completed
+  fi
+  ((temp=temp+1))
+done
