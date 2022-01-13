787897ba184301e89a9062547fe3dd7e5d4c8f04
Arjun Shrivas <97286040+Microservices-delixus@users.noreply.github.com>
2022-01-13 14:23:15 +0530

    Script for apply  slither-analysis on solidity

--- slither_analysis.sh
+++ slither_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 filenames=`ls -l solidity_files | grep '^-' | awk '{print $9}'`
-[ -d "data_log" ] && echo "slither analyzing..." || echo -e " Directory Created \n slither analyzing..." $(mkdir data_log) 
+[ -d "data_log/slither" ] && echo "slither analyzing..." || echo -e " Directory Created \n slither analyzing..." $(mkdir -p data_log/slither) 
 
 for eachfile in $filenames
 do
@@ -8,7 +8,7 @@ do
   then
 
         slither solidity_files/$eachfile &> ${eachfile%.*}_log   # execute slither-analyze on Solidity file and save result inside  log file
-        mv ${eachfile%.*}_log data_log/                # move all log file in data_log folder 
+        mv ${eachfile%.*}_log data_log/slither/                # move all log file in data_log folder 
         echo ${eachfile%.*}_log file created successfully. 
   fi
 
